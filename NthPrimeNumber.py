42681a41c3b5fef0d792389814281d757d484596
Paresh Dandane <Pareshd891@gmail.com>
2018-08-31 17:49:31 +0530

    comments added.

--- NthPrimeNumber.py
+++ NthPrimeNumber.py
@@ -1,9 +1,11 @@
+# helper funtion to check number is prime or not.
 def isprime(N):
     for i in range(2,int(N**0.5)+1):
         if N % i == 0:
             return False
     return True
-        
+
+#function to find out Nth prime number.        
 def nth_prime(N):
     if N==1:
         return 2
